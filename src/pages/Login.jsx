597a40167fb23a1aff505529a93e6c785affaf44
Robenilson <robensilva66@gmail.com>
2024-05-28 10:21:51 -0300

    8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import  {  useState } from 'react';
+import axios from 'axios';
 import Card from '../components/Card';
+
+
 
 
 
@@ -13,13 +16,57 @@ function Login() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Impede o comportamento padrão do formulário de recarregar a página
+  const [error, setError] = useState(null);
 
-    // Exibindo os valores dos campos no console
-    console.log('Email:', email);
-    console.log('Senha:', password);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
+
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(null);
+
+    axios.post("https://localhost:44363/login?email="+email+"&password="+password+"/")
+    .then(response => {
+      console.log('Login bem-sucedido:', response.data);
+      // redirecionar para outra página ou armazenar o token, conforme necessário
+    })
+    .catch(error => {
+      console.error('Erro ao fazer login:', error);
+      setError('Credenciais inválidas. Tente novamente.');
+    });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
        <Card>
@@ -28,17 +75,22 @@ function Login() {
               <form  onSubmit={handleSubmit} >
                 <div className="form-group ">
                   <label htmlFor="email ">Email</label>
-                  <input type="email" className="form-control my-2 mb-4" id="email"    value={email}   onChange={(e) => setEmail(e.target.value)}  placeholder="Seu email" />              
+                  <input type="email" className="form-control my-2 mb-4" id="email"    value={email}   onChange={handleEmailChange} required  placeholder="Seu email" />              
                 </div>
                 <div className="form-group">
                   <label htmlFor="password">Senha</label>
-                  <input type="password" className="form-control my-2  mb-4" id="password"  value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Sua senha" />
+                  <input type="password" className="form-control my-2  mb-4" id="password"  value={password} onChange={handlePasswordChange} required  placeholder="Sua senha" />
                 </div>                
               <div className="form-group text-center  m-0 mb-1">
               </div>             
               <div className="form-group text-center">
-              <Link   to="/Home" className=" text-black text-decoration-none small">
-              <button     className=" btn btn-primary btn-block ">Entrar</button>
+
+              
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+                <button type="submit">Login</button>
+            
+                <Link   to="/Home" className=" text-black text-decoration-none small">
+
               </Link>
                 
                </div>               
